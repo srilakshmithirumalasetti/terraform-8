b3b8a493851f5b44c9edff48c61173deaa67372d
srilakshmithirumalasetti <srilakshmithirumalasetti2@gmail.com>
2025-01-07 11:47:29 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-  ami                   ="ami-0166fe664262f664c"
+  ami                   ="ami-0ca9fb66e076a6e32"
   instance_type          = "t2.micro"
   count                  = 1
   key_name               = "prasanna"
